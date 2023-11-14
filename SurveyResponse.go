f3cf429a9882e8e32f620d5a2f7ca4292925336d
caselongo <caselongo@gmail.com>
2023-11-14 19:59:06 +0100

    Update SurveyResponse.go

--- SurveyResponse.go
+++ SurveyResponse.go
@@ -14,6 +14,7 @@ type SurveyResponse struct {
 	SubmittedAt  types.DateTimeString `json:"submitted_at"`
 	Contact      SurveyContact        `json:"contact"`
 	IsNewContact bool                 `json:"is_new_contact"`
+	Results      []SurveyResult       `json:"results"`
 }
 
 type SurveyContact struct {
@@ -24,6 +25,13 @@ type SurveyContact struct {
 	FullName                    string `json:"full_name"`
 	ConsentsToOneToOneMessaging bool   `json:"consents_to_one_to_one_messaging"`
 	AvatarUrl                   string `json:"avatar_url"`
+}
+
+type SurveyResult struct {
+	QuestionId   string `json:"question_id"`
+	QuestionType string `json:"question_type"`
+	Query        string `json:"query"`
+	Answer       string `json:"answer"`
 }
 
 type ListSurveyResponsesConfig struct {
@@ -68,4 +76,30 @@ func (service *Service) ListSurveyResponses(cfg *ListSurveyResponsesConfig) (*[]
 	}
 
 	return &surveyResponses, nil
+}
+
+type GetSurveyResponseConfig struct {
+	SurveyId  string
+	ReponseId string
+}
+
+func (service *Service) GetSurveyResponse(cfg *GetSurveyResponseConfig) (*SurveyResponse, *errortools.Error) {
+	if cfg == nil {
+		return nil, errortools.ErrorMessage("GetSurveyResponseConfig must not be nil")
+	}
+
+	var surveyResponse SurveyResponse
+
+	requestConfig := go_http.RequestConfig{
+		Method:        http.MethodGet,
+		Url:           service.url(fmt.Sprintf("reporting/surveys/%s/responses/%s", cfg.SurveyId, cfg.ReponseId)),
+		ResponseModel: &surveyResponse,
+	}
+
+	_, _, e := service.httpRequest(&requestConfig)
+	if e != nil {
+		return nil, e
+	}
+
+	return &surveyResponse, nil
 }
